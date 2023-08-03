a1e0e350948fabffacf6014da7d4f7b346067223
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2023-08-03 19:16:53 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 208 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        int N = sc.nextInt();
+        for(int i=1; i<=N;i++){
+            for(int j=1;j<=i;j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+	}
+}
