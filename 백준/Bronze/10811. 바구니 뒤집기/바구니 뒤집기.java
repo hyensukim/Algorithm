30b5935c63ab4835a369ffd872781bfa99683c02
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2023-08-06 16:39:53 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 132 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Integer[] arr = new Integer[N];
+
+		for(int i=0; i < arr.length; i++) arr[i] = i+1;
+
+		for(int k=0; k < M; k++){
+			st = new StringTokenizer(br.readLine(), " ");
+			int i = Integer.parseInt(st.nextToken())-1;
+			int j = Integer.parseInt(st.nextToken())-1;
+			while(i < j){
+				int tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+				i++; j--;
+			}
+		}
+		br.close();
+		for(int a : arr){
+			sb.append(a).append(" ");
+		}
+		System.out.println(sb);	
+	}
+}
