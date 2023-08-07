ba97d8968756b9e8a566dd25981da1231f50433a
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2023-08-07 12:04:14 +0900

    [Bronze V] Title: 아스키 코드, Time: 120 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		char N = br.readLine().charAt(0);
+		System.out.println((int)N);
+		br.close();
+	}
+}
