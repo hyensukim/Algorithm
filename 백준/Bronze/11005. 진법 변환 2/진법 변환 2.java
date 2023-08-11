3113bfa3f31dd38c342db5841fdbe05304fe498e
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2023-08-11 10:15:28 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+		int N = Integer.parseInt(st.nextToken());
+
+		int B = Integer.parseInt(st.nextToken());
+
+		StringBuilder sb = new StringBuilder();
+
+		while(N > 0){
+			
+			if(N%B >= 10){
+				char ch = (char)(N % B + 55);
+				sb.append(ch);
+			}else{
+				sb.append(N%B);
+			}
+
+			N /= B;
+		}
+
+		System.out.println(sb.reverse());
+		br.close();
+	}
+}
