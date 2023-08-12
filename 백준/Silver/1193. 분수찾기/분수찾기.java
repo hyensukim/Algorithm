4ef65682932f255b25b5b5e0b0d466fe98af6b1d
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2023-08-12 23:36:40 +0900

    [Silver V] Title: 분수찾기, Time: 148 ms, Memory: 16100 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -8,28 +8,23 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int X = Integer.parseInt(br.readLine()); // 입력값
-		
-		int level = 1;
+		int level = 1;// 레벨
+		int stack = 0;// 누적 합
+		String str = "";// 결과
 
-		int stack = 0;
-
-		String str = "";
-
-		while(true){
-			if(X <= level + stack){
-				if(level%2 == 1){
-					str=(level-(X-stack-1))+"/"+(X-stack);
-					break;
-				}else{
-					str=(X-stack)+"/"+(level-(X-stack-1));
-					break;
-				}
-			}else{
+		if(X == 1){ str="1/1";}
+		else{
+			while(X > level + stack){
 				stack += level;
 				level++;
 			}
-		}
 
+			if(level%2 == 1){// Lv 홀수
+				str = (level + 1 -X + stack) + "/" + (X-stack); //  분자 = 레벨 + 1 - 분모
+			}else{ // Lv 짝수
+				str = (X-stack) + "/" + (level + 1 -X + stack);
+			}
+		}
 		System.out.println(str);
 		br.close();
 	}
