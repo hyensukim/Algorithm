77b1742c1f236753ae3f84fe4409fa545d6630a9
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2023-08-18 22:34:10 +0900

    [Bronze II] Title: 분해합, Time: 148 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int result = 0;
+        
+        for(int i=1; i<= N; i++){
+            int num = 0, sum = 0;
+
+            num = i;
+
+            while(num != 0){
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if(sum + i == N){
+                result = i;
+                break;
+            }
+            
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}
