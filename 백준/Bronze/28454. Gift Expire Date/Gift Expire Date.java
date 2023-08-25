3657e98f8fafffd0eaac7b90b25bb4b5d141b375
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2023-08-25 17:20:36 +0900

    [Bronze III] Title: Gift Expire Date, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/28454. Gift Expire Date/Gift Expire Date.java
+++ 백준/Bronze/28454. Gift Expire Date/Gift Expire Date.java
@@ -1,24 +1,29 @@
 import java.io.*;
-import java.text.*;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args)throws IOException, ParseException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String currentDate = br.readLine();
+        String today = br.readLine();
+        StringTokenizer st = new StringTokenizer(today,"-");
+        int y1 = Integer.parseInt(st.nextToken());
+        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        
         int N = Integer.parseInt(br.readLine());
-        int usableCount = 0;
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        for(int i =0; i < N; i++){
-            String strDate = br.readLine();
-            Date date1 = sdf.parse(currentDate);
-            Date date2 = sdf.parse(strDate);
-            if(date1.compareTo(date2) <= 0){
-                ++usableCount;
-            }
+        int cnt = 0;
+        for(int i=0; i < N; i++){
+            String expiredDate = br.readLine();
+            st = new StringTokenizer(expiredDate,"-");
+            int y2 = Integer.parseInt(st.nextToken());
+            int m2 = Integer.parseInt(st.nextToken());
+            int d2 = Integer.parseInt(st.nextToken());
+            // System.out.printf("%d - %d - %d\n",y2,m2,d2);
+            boolean check = y1 < y2 || (y1==y2 && m1 < m2) || (y1 == y2 && m1 == m2 && d1 <= d2);
+            if(check) cnt++;     
         }
         
-        System.out.println(usableCount);
+        System.out.println(cnt);
         br.close();
     }
 }
