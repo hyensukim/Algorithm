1551ac195590a1ee0b01bf017774673bf2df9f8c
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2023-10-05 00:29:47 +0900

    [Silver II] Title: 스택 수열, Time: 376 ms, Memory: 27876 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        int start = 0;
+        
+        while(N-- > 0){
+            int value = Integer.parseInt(br.readLine());
+            
+            if(value > start){
+                for(int i = start+1; i <= value; i++ ){
+                    stack.push(i);
+                    sb.append("+").append("\n");
+                }
+                start = value;
+            }else if(stack.peek() != value){
+                System.out.print("NO");
+                return;
+            }
+            
+            stack.pop();
+            sb.append("-").append("\n");
+        }
+        
+        System.out.println(sb);
+        br.close();
+    }
+}
