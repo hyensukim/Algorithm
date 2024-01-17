b9cecb576b634c4399a4c504648de894c228db2d
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-01-17 23:46:09 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 604 ms, Memory: 294828 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,33 +1,33 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Main{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] argas)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+        
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        Queue<Integer> queue = new LinkedList<>();
-        
-        for(int i=1; i<=N; i++){
-            queue.offer(i);
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1; i <= N; i++){
+            q.offer(i);
         }
         
-        StringBuilder sb = new StringBuilder();
-        sb.append("<");
-        
-        while(queue.size() > 0){
-            
-            for(int i = 0; i < K-1; i++){
-                 queue.offer(queue.poll());   
+        StringBuilder sb = new StringBuilder("<");
+        while(!q.isEmpty()){
+            for(int i=0; i < K-1; i++){
+                q.offer(q.poll());
             }
-            
-            sb.append(queue.poll()).append(", ");
+            Integer i = q.poll();
+            sb.append(i).append(", ");
         }
         
-        int idx = sb.lastIndexOf(",");
-		StringBuilder sb2 = new StringBuilder(sb.substring(0,idx));
-		sb2.append(">");
-        System.out.println(sb2);
+        sb.replace(sb.length()-2,sb.length(),">");
+        System.out.println(sb);
     }
 }
