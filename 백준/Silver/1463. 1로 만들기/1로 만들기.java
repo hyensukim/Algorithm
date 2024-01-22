490a6c54365ee952b5eabc69459e09ace0d2e934
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-01-23 01:54:20 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,47 +1,22 @@
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
-
-public class Main{
-    
-    static Integer[] dp;
-    
-    public static void main(String[] args)throws IOException{
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        br.close();
-        
-        dp = new Integer[N+1];
-        dp[0] = dp[1] = 0;
-        
-        System.out.println(recur(N));
-    }
-    
-    static int recur(int N){
-        
-        if(Objects.isNull(dp[N])){
-            // 6으로 나눠지는 경우
-            if(N % 6 == 0){
-                dp[N] = Math.min(recur(N-1),Math.min(recur(N/3),recur(N/2)))+1;
-            }
-            // 3으로만 나눠지는 경우
-            else if(N % 3 == 0){
-                dp[N] = Math.min(recur(N/3), recur(N-1))+1;
-            }
-            // 2로만 나눠지는 경우
-            else if(N % 2 == 0){
-                dp[N] = Math.min(recur(N/2), recur(N-1))+1;
-            }
-            // 2와 3으로 나뉘지 않는 경우
-            else{
-                dp[N] = recur(N-1)+1;
-            }
-        }
-        
-        return dp[N];
-    }
+import java.io.IOException;
+ 
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(recur(N, 0));
+	}
+ 
+	static int recur(int N, int count) {
+		// N이 2 미만인 경우 누적된 count값을 반환
+		if (N < 2) {
+			return count;
+		}
+		return Math.min(recur(N / 2, count + 1 + (N % 2)), recur(N / 3, count + 1 + (N % 3)));
+ 
+	}
 }
