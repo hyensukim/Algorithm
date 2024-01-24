bdbdfd0751276d5fe0239c53a4b3193cef751998
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-01-24 09:33:58 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,5 +1,6 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
     public static void main(String[] args)throws IOException{
