e3e3a9077857c0b9790ee3333d50a9c88aff37db
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-02-08 13:53:04 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1856 ms, Memory: 300840 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,24 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class Main{
-    public static void main(String[] args)throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        
-        for(int i=0; i < N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()); // 입력 개수
+
+        // 입력값 범위(0~10000)에 해당하는 배열 생성
+        int[] arr = new int[10001];
+
+        // 입력 받은 수를 배열의 인덱스로 카운팅
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[num]++;
         }
-        
-        Arrays.sort(arr);
-        
-        StringBuilder sb = new StringBuilder();
-        for(int a : arr){
-            sb.append(a).append("\n");
+
+        // 배열의 인덱스를 순회하며 카운팅된 수만큼 출력
+        for (int i = 0; i < 10001; i++) {
+            while (arr[i] > 0) {
+                bw.write(Integer.toString(i) + "\n");
+                arr[i]--;
+            }
         }
-        
-        System.out.println(sb);
+
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
