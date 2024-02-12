6d110dd636badc8b12a72522e1f4b4e15214f5cc
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-02-12 20:39:10 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        
+        if(N%2 == 0){
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+    }
+}
