49d3595efec06fd742db27eeaa6433794799ddef
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-02-24 21:36:16 +0900

    [Silver IV] Title: 수 찾기, Time: 604 ms, Memory: 44572 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,18 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static StringBuilder sb = new StringBuilder();
     static int[] a;
+    static int n;
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         
         a = new int[n];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         
-        for(int i=0; i<n; i++){
+        for(int i=0; i < n; i++){
             a[i] = Integer.parseInt(st.nextToken());
         }
         
@@ -25,37 +25,41 @@ public class Main{
         
         br.close();
         
+        StringBuilder sb = new StringBuilder();
+        
         for(int i=0; i<m; i++){
-            int k = Integer.parseInt(st.nextToken());
-            halfSearch(k,0,n-1);
+            int target = Integer.parseInt(st.nextToken());
+            
+            if(halfSearch(target)){
+                sb.append(1).append("\n");
+            }
+            else{
+                sb.append(0).append("\n");
+            }
+            
         }
         
         System.out.println(sb);
     }
     
-    static void halfSearch(int find, int start, int end){
-        
-        if(start <= end){
-            
-            int half = (start + end)/2;
-            
-            if(a[half] > find){
-                halfSearch(find,start, half-1);
+    static boolean halfSearch(int target){
+        int start = 0;
+        int end = n-1;
+
+        while(start <= end){
+
+            int half = (start + end) / 2;
+
+            if(a[half] > target){
+                end = half-1;
             }
-            else if(a[half] < find){
-                halfSearch(find,half+1, end);
+            else if(a[half] < target){
+                start = half+1;
             }
             else{
-                sb.append(1).append("\n");
-                return;
+                return true;
             }
-            
         }
-        else{
-            
-            sb.append(0).append("\n");
-            return;
-            
-        }
+        return false;
     }
 }
