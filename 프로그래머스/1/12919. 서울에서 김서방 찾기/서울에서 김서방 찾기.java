28a9e6b66a4fc63f756be57e7a6c8be4a3ff2729
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-03-11 17:30:21 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.45 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        
+        int x = Arrays.asList(seoul).indexOf("Kim");
+        
+        return answer = "김서방은 " + x + "에 있다";
+    }
+}
