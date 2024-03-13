5c537aa95b8eb36b6d93b5d0978685f8e065bd98
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-03-13 14:45:21 +0900

    [level 1] Title: 시저 암호, Time: 31.43 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        char[] arr = s.toCharArray();
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] >= 65 && arr[i] <= 90){ // 대문자
+                arr[i] += n;
+                
+                while(arr[i] > 90){
+                    int tmp = arr[i] - 91;
+                    arr[i] = (char)(65 + tmp);
+                }
+            }
+            else if(arr[i] >= 97 && arr[i] <= 122){ //소문자
+                arr[i] += n;
+                
+                while(arr[i] > 122){
+                    int tmp = arr[i] - 123;
+                    arr[i] = (char)(97 + tmp);
+                }
+            }
+            answer += arr[i];            
+        }
+        
+        return answer;
+    }
+}
