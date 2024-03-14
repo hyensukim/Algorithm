327fa5bd82220e477038b1e6fab31a33fdaaee8f
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-03-14 18:43:54 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        int len = nums.length;
+
+        for(int i=0; i< len-2; i++){
+            for(int j=i+1; j < len-1; j++){
+                for(int k =j+1; k < len; k++){
+
+                    int sum = nums[i] + nums[j] + nums[k];
+
+                    if(isPrime(sum)){
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public boolean isPrime(int num){
+        
+        for(int i=2; i < num; i++){
+            if(num%i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
