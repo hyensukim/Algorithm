03cf72f3887e330bb08e40843c1e41e5ae42eca4
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-03-15 23:39:13 +0900

    [level 1] Title: 문자열 나누기, Time: 0.68 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,31 +1,26 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-        
-        int match = 0;
-        
-        int not_match = 0;
-        
-        char first = s.charAt(0);
-        
-        for(int i=0 ; i<s.length(); i++){
-            if(first == s.charAt(i)){
-                match++;
+        char x = s.charAt(0);
+        int xNum = 0;
+        int notXNum = 0;
+        int answer = 1;
+        for (int i=0;i<s.length();i++){
+            if (x == s.charAt(i)){
+                xNum++;
+            } else {
+                notXNum++;
             }
-            else{
-                not_match++;
-            }
-            
-            if(match == not_match){
-                answer++;
-                if(i+1 < s.length()){
-                    first = s.charAt(i+1);
-                }
+            if (xNum == notXNum){
+                if (i==s.length()-1){
+                    
+                } else{
+                    x = s.charAt(i+1);
+                    xNum = 0;
+                    notXNum = 0;
+                    answer++;
+                }            
             }
         }
-        
-        if(match != not_match)
-            answer++;
         
         return answer;
     }
