1df6c60d628fc61de328952ca295b0c6e50e2045
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-03-19 13:19:31 +0900

    [level 1] Title: 둘만의 암호, Time: 17.58 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        
+        for(char c : s.toCharArray()){
+            for(int i=0; i<index; i++){
+                c += 1;
+                
+                if(c > 'z')
+                    c -= 26;
+                
+                if(skip.contains(c + ""))
+                    i--;
+            }
+            answer += c;
+        }
+        
+        return answer;
+    }
+}
