afeae9b6930d01f9492aa756243e45ba28f415f7
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-03-28 09:22:17 +0900

    [Bronze II] Title: 단어의 개수, Time: 268 ms, Memory: 20044 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,25 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		int cnt = 0;
-
-		while(st.hasMoreTokens()){
-			if(st.nextToken() != null){
-				cnt++;
-			}
-		}
-
-		br.close();
-
-		System.out.println(cnt);
-	}
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        System.out.println(st.countTokens());
+        
+        br.close();
+    }
 }
