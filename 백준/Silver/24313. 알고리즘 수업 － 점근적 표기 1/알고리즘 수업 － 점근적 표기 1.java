4e0bdba2e25318d4a2135ff7d8cfb56dae63fbc9
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-03-29 15:07:30 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -11,9 +11,9 @@ public class Main {
         int a1 = Integer.parseInt(sArr[0]);
         int a0 = Integer.parseInt(sArr[1]);
         int c = Integer.parseInt(br.readLine());
-        int n = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
 
-        if (a1 <= c && 0 <= (c - a1) * n - a0) {
+        if (a1 <= c && 0 <= (c - a1) * n0 - a0) {
             System.out.println(1);
         } else {
             System.out.println(0);
