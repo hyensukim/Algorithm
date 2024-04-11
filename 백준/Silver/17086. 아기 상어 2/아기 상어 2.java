37bcc16f9565c4bb1f8d939901680328cfd4c199
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-04-11 11:54:09 +0900

    [Silver II] Title: 아기 상어 2, Time: 444 ms, Memory: 175408 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -1,56 +1,102 @@
 import java.io.*;
 import java.util.*;
+/**
+ * 1. 문제 파악
+ * 두 칸의 거리는 하나의 칸에서 다른 칸으로 가기 위해서 지나야 하는 칸의 수.
+ * 이동 방향 8방향 - 델타배열
+ * 안전 거리는 가장 가까운 아기 상어와의 거리
+ * 
+ * N,M : 2~50
+ * 0 - 빈칸, 1 - 아기상어
+ * 
+ * 안전거리의 최댓값을 출력
+ * 
+ * 2. 접근 방법
+ * 각 아기 상어의 위치에서 bfs를 수행합니다.(X)
+ * 안전거리를 확인하기 위한 배열을 생성합니다. - int 배열
+ * 안전거리 값이 겹치는 경우, 둘 중 최소값을 기준으로 안전거리를 넣습니다.
+ * 1이 아니고 배열 범위 안에서만 이동
+ * 안전 거리 중에서 최댓값을 출력 해줍니다.
+ * 
+ * 2-1
+ * 빈칸에서 bfs를 수행합니다.
+ * 
+ */
 public class Main {
-    static int arr[][];
-    static boolean check[][];
-    static int n,m;
-    static int dx[]= {0,0,1,-1,1,1,-1,-1};
-	static int dy[]= {1,-1,0,0,1,-1,-1,1};
-    
-    static int bfs(int a,int b){
-        check=new boolean[n+1][m+1];
-        Queue<int[]> q=new LinkedList();
-        q.add(new int[]{a,b,0});
-        check[a][b]=true;
-        
-        while(!q.isEmpty()){
-            int v[]=q.poll();
-            
-            for(int i=0;i<8;i++){
-                int x=v[0]+dx[i];
-                int y=v[1]+dy[i];
-                int z=v[2]+1;
-                
-                if(x<0 || x>=n || y<0 || y>=m || check[x][y]==true) continue;
-                if(arr[x][y]==1) return z;
-                q.add(new int[]{x,y,z});
-                check[x][y]=true;
+    static int[][] board;
+    static boolean[][] visited;
+
+    static int dr[]= {0,0,1,-1,1,1,-1,-1};
+	static int dc[]= {1,-1,0,0,1,-1,-1,1};
+    static int row,col;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+
+        board = new int[row][col];
+
+        for(int r=0; r<row; r++){
+            st = new StringTokenizer(br.readLine());
+            for(int c=0; c<col; c++){
+                board[r][c] = Integer.parseInt(st.nextToken());            
             }
         }
+
+        int tmp = 0;
+        int count = 0;
+        for(int r=0; r<row; r++){
+            for(int c=0; c<col; c++){
+                if(board[r][c] != 1){
+                    tmp = bfs(r,c);
+                    count = Math.max(count,tmp);
+                }
+            }
+        }
+
+        br.close();
+
+        System.out.println(count);
+    }
+
+    static int bfs(int r, int c){
+        visited = new boolean[row][col];
+        Queue<Coordinator> q = new LinkedList<>();
+        q.offer(new Coordinator(r, c, 0));
+        visited[r][c] = true;
+
+        while(!q.isEmpty()){
+            Coordinator now = q.poll();
+
+            for(int i=0; i<8; i++){
+                int tr = now.cr + dr[i];
+                int tc = now.cc + dc[i];
+                int cnt = now.count + 1;
+
+                if(tr < 0 || tc < 0 || tr >= row || tc >= col || visited[tr][tc]) continue;
+
+                if(board[tr][tc] == 1) return cnt;
+
+                q.offer(new Coordinator(tr,tc,cnt));
+
+                visited[tr][tc] = true;
+            }
+        }
+
         return 0;
     }
-    
-    public static void main(String args[]) {
-        Scanner s=new Scanner(System.in);
-        n=s.nextInt();
-        m=s.nextInt();
-        arr=new int[n+1][m+1];
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr[i][j]=s.nextInt();
-            }
+
+    static class Coordinator{
+        int cr; // Coordinator-Row
+        int cc; // Coordinator-Col
+        int count; // idea
+
+        Coordinator(int cr, int cc, int count){
+            this.cr = cr;
+            this.cc = cc;
+            this.count = count;
         }
-        int count=0;
-        int num=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]!=1){
-                    num=bfs(i,j);
-                    count=num>count ? num : count;
-                } 
-            }
-        }
-        System.out.print(count);
     }
 }
