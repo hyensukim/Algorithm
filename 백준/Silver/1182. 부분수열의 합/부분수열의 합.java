42300b39433d994a953b951f3f167c10532ee52e
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-04-14 16:35:21 +0900

    [Silver II] Title: 부분수열의 합, Time: 244 ms, Memory: 14880 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int[] arr;
+    static boolean[] visited;
+    static int n,s,result;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        visited = new boolean[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1; i<=n; i++){
+            combination(0,i);
+        }
+
+        br.close();
+
+        System.out.println(result);
+    }
+
+    static void combination(int start, int r){ // r : 조합의 갯수
+        if(r == 0){
+            int sum = 0;
+            for(int i=0; i<n; i++){
+                if(visited[i]){
+                    sum += arr[i];
+                }
+            }
+
+            if(sum == s){
+                result++;
+            }
+
+            return;
+        }
+
+        for(int i=start; i<n; i++){
+            visited[i] = true;
+            combination(i+1,r-1);
+            visited[i] = false;
+        }
+    }
+}
