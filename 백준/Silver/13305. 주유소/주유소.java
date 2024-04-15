8612ed08682f9a07fa0b7b60ba02bfed5f4d1c8d
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-04-15 09:56:04 +0900

    [Silver III] Title: 주유소, Time: 476 ms, Memory: 36492 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -5,40 +5,44 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] dist = new int[n-1];
-        int[] fare = new int[n];
+        long[] dist = new long[n-1];
+        long[] fare = new long[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<n-1; i++){
-            dist[i] = Integer.parseInt(st.nextToken());
+            dist[i] = Long.parseLong(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
-            fare[i] = Integer.parseInt(st.nextToken());
+            fare[i] = Long.parseLong(st.nextToken());
         }
 
-        long remain = 0;
+        long min = fare[0];
         long sum = 0;
         for(int i=0; i<n-1; i++){
-            if(dist[i] > remain){
-                if(fare[i] > fare[i+1]){
-                    sum += (fare[i] * dist[i]);
-                    remain += dist[i];
-                }
-                else{
-                    if(i < n-2){
-                        sum += (fare[i] * dist[i] + fare[i] * dist[i+1]);
-                        remain += (dist[i] + dist[i+1]);
-                    }
-                    else{
-                        sum += (fare[i] * dist[i]);
-                        remain += dist[i];
-                    }
-                }
+            if(fare[i] < min){
+                min = fare[i];
             }
+            sum += (min * dist[i]);
+            // if(dist[i] > remain){
+            //     if(fare[i] > fare[i+1]){
+            //         sum += (fare[i] * dist[i]);
+            //         remain += dist[i];
+            //     }
+            //     else{
+            //         if(i < n-2){
+            //             sum += (fare[i] * dist[i] + fare[i] * dist[i+1]);
+            //             remain += (dist[i] + dist[i+1]);
+            //         }
+            //         else{
+            //             sum += (fare[i] * dist[i]);
+            //             remain += dist[i];
+            //         }
+            //     }
+            // }
 
-            remain -= dist[i];
+            // remain -= dist[i];
         }
 
         br.close();
