fe481b557f67fa70d86a319a78283bbe3737ef8d
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-08-16 00:55:14 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,34 +1,34 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
+
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main{
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         List<Integer> list = new ArrayList<>();
-        long sum = 0L;
+        int sum = 0;
         
         for(int i=0; i<9; i++){
-            Integer key = Integer.parseInt(br.readLine());
+            int key = Integer.parseInt(br.readLine());
             list.add(key);
             sum += key;
         }
         
         for(int i=0; i<9; i++){
             for(int j=i+1; j<9; j++){
-                if(sum - (list.get(i) + list.get(j)) == 100){
+                if(sum - list.get(i) - list.get(j) == 100){
                     list.set(i,0);
                     list.set(j,0);
                     Collections.sort(list);
-                    list.stream().filter(x -> x != 0).forEach(System.out::println);
+                    list.stream().filter(x -> x > 0).forEach(System.out::println);
                     return;
                 }
             }
         }
-        
         br.close();
     }
 }
