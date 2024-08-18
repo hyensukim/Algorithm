1059e4b212e60b58317dc45f0e2c06fd3849ad60
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-08-18 12:19:44 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = br.readLine().toCharArray();
+        int[] nums = new int[10];
+        
+        for(char ch : chars) {
+            nums[ch-48]++;
+        }
+        
+        int res = 1;
+        for(int i = 0; i < 10; i++){
+            if(res < nums[i]){
+                if(i == 6 || i == 9){
+                    res = (int)Math.ceil((nums[6] + nums[9]) / 2F);
+                } else {
+                    res = nums[i];
+                }
+            }
+        }
+        
+        System.out.println(res);
+        br.close();
+    }
+}
