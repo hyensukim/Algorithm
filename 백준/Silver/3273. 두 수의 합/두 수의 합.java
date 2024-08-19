f71468007bf56cba4be198e0a0cf6d216fd27e83
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-08-19 14:10:19 +0900

    [Silver III] Title: 두 수의 합, Time: 300 ms, Memory: 28536 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long x = Long.parseLong(br.readLine());
+        
+        long[] nums = new long[n];
+        for(int i=0; i<n; i++) {
+            nums[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(nums);
+        
+        int flagS = 0;
+        int flagE = n-1;
+        int count = 0;
+        while(flagS < flagE) {
+            long sum = nums[flagS] + nums[flagE];
+            if (x == sum) {
+                count++;
+                flagS++;
+                flagE--;
+            } else if (x > sum) {
+                flagS++;
+            } else {
+                flagE--;
+            }
+        }
+        
+        System.out.println(count);
+        br.close();
+    }
+}
