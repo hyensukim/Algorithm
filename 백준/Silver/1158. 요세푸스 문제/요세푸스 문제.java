ff101d4dee5ad3219889f80ab233a4d5a84e3d8b
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-09-13 12:22:34 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 116 ms, Memory: 14952 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -3,41 +3,38 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import java.util.Queue;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder("<");
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Queue<Integer> q = new LinkedList<>();
-        
-        int n = Integer.parseInt(st.nextToken());
-        for(int i=1; i<=n; i++) {
-            q.offer(i);
-        }
-        
+        StringBuilder sb = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
         Queue<Integer> answer = new LinkedList<>();
-        int k = Integer.parseInt(st.nextToken());
-        while(!q.isEmpty()){
-            if(q.peek() == null){
-                break;
-            }
-            
-            for(int i=0; i<k-1; i++) {
-                q.offer(q.poll());
-            }
-            
-            answer.offer(q.poll());
+        int N = Integer.parseInt(st.nextToken());
+        for (int i=0; i<N; i++) {
+            list.add(i+1);
         }
-        br.close();
-        
-        while(answer.size() > 1) {
+        int K = Integer.parseInt(st.nextToken());
+
+        int idx = 0;
+        while (!list.isEmpty()) {
+            idx = (idx+K-1)%list.size();
+            answer.add(list.remove(idx));
+        }
+
+        sb.append("<");
+        while (!answer.isEmpty()) {
             sb.append(answer.poll());
-            sb.append(", ");
+            if (!answer.isEmpty()) {
+                sb.append(", ");
+            }
         }
-        sb.append(answer.poll()).append(">");
-        System.out.println(sb.toString());
+        sb.append(">");
+        System.out.println(sb);
     }
 }
