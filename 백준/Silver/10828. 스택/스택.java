3c1f3d1afae2d54fff506488e00f98e435037259
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-09-14 17:20:22 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 19816 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,42 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main{
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
+    public static void main(String[] args)throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder out = new StringBuilder();
         
-        for(int i=0; i < N; i++){
-            String str = br.readLine();
-            if(str.contains("push")){
-                String tmp = str.substring(5);
-                int X = Integer.parseInt(tmp);
-                stack.push(X);
-            }else if(str.equals("pop")){
-                if(!stack.empty()){
-                    Integer num = stack.pop();
-                    sb.append(num).append("\n");
-                }else{
-                    sb.append("-1").append("\n");
-                }
-            }else if(str.equals("size")){
-                sb.append(stack.size()).append("\n");
-            }else if(str.equals("empty")){
-                int tmp = 0;
-                tmp = stack.empty() ? 1 : 0;
-                sb.append(tmp).append("\n");
-            }else{ // top
-                if(!stack.empty()){
-                    Integer num = stack.peek();
-                    sb.append(num).append("\n");
-                }else{
-                    sb.append("-1").append("\n");
-                }
+        int n = Integer.parseInt(in.readLine());
+        
+        Deque<Integer> deq = new ArrayDeque<>();
+        while(n-- > 0) {
+            String input = in.readLine();
+            StringTokenizer st = new StringTokenizer(input, " ");
+            String command = st.nextToken();
+            
+            if(("pop".equals(command) || "top".equals(command)) 
+               && deq.size() < 1) {
+                out.append(-1).append("\n");
+                continue;
+            }
+            
+            switch(command) {
+                case "pop" : 
+                    out.append(deq.pop()).append("\n");
+                    break;
+                case "size" :
+                    out.append(deq.size()).append("\n");
+                    break;
+                case "empty" :
+                    if(deq.size() < 1) {
+                        out.append(1).append("\n");
+                    } else {
+                        out.append(0).append("\n");
+                    }
+                    break;
+                case "top" :
+                    out.append(deq.peek()).append("\n");
+                    break;
+                default :
+                    int i = Integer.parseInt(st.nextToken());
+                    deq.push(i);
+                    break;
             }
         }
-        System.out.println(sb);
-        br.close();
+        
+        in.close();
+        
+        System.out.println(out);
     }
 }
