1af3fa4eb03db193cc8cbe01975618db79f09d04
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-09-19 17:13:05 +0900

    [Silver II] Title: 스택 수열, Time: 248 ms, Memory: 26696 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,35 +1,33 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class Main{
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int[] stack = new int[N];
-        int idx = 0;
-        int start = 0;
         
-        while(N-- > 0){
-            int value = Integer.parseInt(br.readLine());
-            
-            if(value > start){
-                for(int i = start+1; i <= value; i++ ){
-                    stack[idx] = i;
-                    idx++;
-                    sb.append("+").append("\n");
+        int n = Integer.parseInt(in.readLine());
+        int[] stack = new int[n+1];
+        int idx = 0;
+        int s = 0;
+
+        while(n-- > 0) {
+            int v = Integer.parseInt(in.readLine());
+            if(s < v) {
+                for(int i=s+1; i<=v; i++) {
+                    stack[++idx] = i;
+                    sb.append('+').append('\n');
                 }
-                start = value;
-            }else if(stack[idx-1] != value){
-                System.out.print("NO");
+                s = v;
+            } else if(stack[idx] != v) {
+                System.out.println("NO");
                 return;
             }
-            
             idx--;
-            sb.append("-").append("\n");
+            sb.append('-').append('\n');
         }
-        
+        in.close();
         System.out.println(sb);
-        br.close();
     }
 }
