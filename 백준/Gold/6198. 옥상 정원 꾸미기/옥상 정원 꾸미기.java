2cad2aa80eaf053f8643bb9c816ff5d3d1bff500
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-09-22 16:44:55 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 200 ms, Memory: 21872 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    
+    public static long sum;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] stack = new int[N];
+        int top = -1;
+        sum = 0;
+        for(int i=0; i<N; i++){
+            int now = Integer.parseInt(br.readLine());
+            while(top > -1) {
+                if(stack[top] <= now) {
+                    top--;
+                } else {
+                    break;
+                }
+            }
+            sum += ++top;
+            stack[top] = now;
+        }
+
+        System.out.println(sum);
+        br.close();
+    }
+}
