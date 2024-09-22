ef14a6a0e553284f0d5b6453a71cf1b0cf6552ca
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-09-22 15:19:01 +0900

    [Gold V] Title: 탑, Time: 632 ms, Memory: 89572 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,46 +1,41 @@
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        Tower[] towers = new Tower[n];
-        int top = -1; // 스택의 top을 가리키는 인덱스
-        
-        for (int i = 1; i <= n; i++) {
-            int value = Integer.parseInt(st.nextToken());
-            
-            while (top >= 0 && value > towers[top].high) {
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Tower[] stack = new Tower[N];
+        int top = -1;
+        for(int i=1; i<=N; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            while(top >= 0 && stack[top].high < now) {
                 top--;
             }
-            
-            if (top == -1) {
+            if(top == -1) {
                 sb.append(0).append(" ");
             } else {
-                sb.append(towers[top].idx).append(" ");
+                sb.append(stack[top].idx).append(" ");
             }
             
-            towers[++top] = new Tower(i, value);
+            stack[++top] = new Tower(i,now);
         }
-        
-        System.out.println(sb);
         br.close();
+        System.out.println(sb);
     }
 }
 
-class Tower {
-    public int idx;
-    public int high;
-    
-    public Tower(int idx, int high) {
-        this.idx = idx;
-        this.high = high;
-    }
+class Tower{
+        public int idx;
+        public int high;
+        
+        public Tower(int idx, int high){
+            this.idx = idx;
+            this.high = high;
+        }
 }
