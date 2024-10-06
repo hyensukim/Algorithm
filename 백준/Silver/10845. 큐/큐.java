7801f44c54d28c7ea3700df1113f68b551eb2370
Kim Hyen Su <118423671+hyensukim@users.noreply.github.com>
2024-10-06 15:59:12 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 19652 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,62 +1,51 @@
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.LinkedList;
+import java.io.BufferedReader;
 
-public class Main{
-    public static void main(String[] args)throws IOException{
+import java.util.StringTokenizer;
+import java.util.Deque;
+import java.util.ArrayDeque;
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         
         int N = Integer.parseInt(br.readLine());
         
-        LinkedList<String> queue = new LinkedList<>();
-        
-        while(N-- > 0){
+        Deque<Integer> queue = new ArrayDeque<>();
+        while(N-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            while(st.hasMoreTokens()){
-                String str = st.nextToken();
-                
-                switch(str){
-                    case "push":
-                        queue.add(st.nextToken());
-                        break;
-                    case "pop":
-                        if(!queue.isEmpty()){
-                            sb.append(queue.poll());
-                        }else{
-                            sb.append(-1);
-                        }
-                        break;
-                    case "size":
-                        sb.append(queue.size());
-                        break;
-                    case "empty":
-                        if(queue.isEmpty()){
-                            sb.append(1);
-                        }else{
-                            sb.append(0);
-                        }
-                        break;
-                    case "front":
-                        if(!queue.isEmpty()){
-                            sb.append(queue.peekFirst());
-                        }else{
-                            sb.append(-1);
-                        }
-                        break;
-                    case "back":
-                        if(!queue.isEmpty()){
-                            sb.append(queue.peekLast());
-                        }else{
-                            sb.append(-1);
-                        }
-                        break;
-                }
-                if(!str.equals("push")){
-                    sb.append("\n");
-                }
+            String command = st.nextToken();
+            switch(command) {
+                case "push" :
+                    int value = Integer.parseInt(st.nextToken());
+                    queue.offer(value); break;
+                case "pop" :
+                    if(queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.poll()).append("\n");
+                    } break;
+                case "size" :
+                    sb.append(queue.size()).append("\n"); break;
+                case "empty" :
+                    if(queue.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    } break;
+                case "front" :
+                    if(!queue.isEmpty()) {
+                        sb.append(queue.peekFirst()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    } break;
+                default :
+                    if(!queue.isEmpty()) {
+                        sb.append(queue.peekLast()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
             }
         }
         br.close();
