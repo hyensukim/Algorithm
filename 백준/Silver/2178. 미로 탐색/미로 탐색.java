02aabf40eef60640df37cff573e790fada7d9a2c
hyensukim <118423671+hyensukim@users.noreply.github.com>
2024-12-01 21:14:03 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,52 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.ArrayDeque;
 
 public class Main{
-    static boolean[][] visited;
-    static int[][] arr;
-    static int[] dr = {0,1,-1,0};
-    static int[] dc = {1,0,0,-1};
-    static int n,m;
+    static int[][] graph;
+    
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); // 행 -
-        m = Integer.parseInt(st.nextToken()); // 열 |
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        graph = new int[N][M];
         
-        visited = new boolean[n][m];
-        arr = new int[n][m];
-        
-        for(int i=0; i<n; i++){
-            String str = br.readLine();
-            for(int j=0; j<m; j++){
-                arr[i][j] = Integer.parseInt(str.charAt(j) + "");
+        for(int i=0; i<N; i++){
+            String line = br.readLine();
+            for(int j=0; j<M; j++){
+                graph[i][j] = line.charAt(j) - '0';
             }
         }
-        br.close();
-        
-        bfs(0,0);
-        
-        System.out.println(arr[n-1][m-1]);
+        bfs();
+        System.out.println(graph[N-1][M-1]);
     }
     
-    static void bfs(int r, int c){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {r,c});
-        visited[r][c] = true;
-        
-        while(!queue.isEmpty()){
-            int[] now= queue.poll();
-            
+    static void bfs(){
+        int[] dx = {1,-1,0,0};
+        int[] dy = {0,0,1,-1};
+        Queue<int[]> que = new ArrayDeque<>();
+        que.offer(new int[]{0,0});
+        while(!que.isEmpty()){
+            int[] now = que.poll();
+            int x = now[0];
+            int y = now[1];
             for(int i=0; i<4; i++){
-                int tmp_row = now[0] + dr[i];
-                int tmp_col = now[1] + dc[i];
-                
-                if(tmp_row >= 0 && tmp_col >= 0 && tmp_row < n && tmp_col < m){
-                    if(arr[tmp_row][tmp_col] != 0 && !visited[tmp_row][tmp_col]){
-                        visited[tmp_row][tmp_col] = true;
-                        arr[tmp_row][tmp_col] = arr[now[0]][now[1]] + 1;
-                        queue.offer(new int[] {tmp_row, tmp_col});
-                    }
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx < 0 || ny < 0 || nx >= graph.length || ny >= graph[0].length){
+                    continue;
+                }
+                if(graph[nx][ny] == 1){
+                    graph[nx][ny] += graph[x][y];
+                    que.offer(new int[]{nx,ny});
                 }
             }
         }
