aa9bc8ad3cd57085b74cda4bfc2699cc83936c1c
Hyensu <118423671+hyensukim@users.noreply.github.com>
2025-01-20 22:46:38 +0900

    [Bronze II] Title: 단어의 개수, Time: 176 ms, Memory: 20300 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.kt
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.kt
@@ -1,10 +1,9 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.StringTokenizer
 
 fun main(){
     val input = BufferedReader(InputStreamReader(System.`in`))
-    val line = input.readLine().trim()
-    val wordList = line.split(" ")
-    if(wordList.size == 1) if(wordList[0].isBlank()) println(0) else println(1) 
-    else println(wordList.size)
+    val token = StringTokenizer(input.readLine())
+    print(token.countTokens())
 }
