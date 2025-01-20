68fa011c6cf205fcd1e87a5fad302cbf6595dc1d
Hyensu <118423671+hyensukim@users.noreply.github.com>
2025-01-20 21:55:17 +0900

    [Bronze V] Title: ??!, Time: 76 ms, Memory: 11908 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.kt
@@ -0,0 +1,7 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(){
+    val input = BufferedReader(InputStreamReader(System.`in`))
+    println("${input.readLine()}??!")
+}
